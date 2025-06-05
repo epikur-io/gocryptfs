8e1ffb01c56de3e8f86ecc5f17d2f415c6c2bbbd
Jakob Unterwurzacher <jakobunt@gmail.com>
2025-06-05 19:57:06 +0200

    build-without-openssl.bash: use ld.so --verify instead of ldd
    As reported in https://github.com/rfjakob/gocryptfs/issues/926 ,
    some ldd version don't report an error when encountering a static
    executable.
    
    Use "ld.so --verify" instead.

--- build-without-openssl.bash
+++ build-without-openssl.bash
@@ -4,7 +4,9 @@ cd "$(dirname "$0")"
 
 CGO_ENABLED=0 source ./build.bash -tags without_openssl
 
-if ldd gocryptfs 2> /dev/null ; then
+# ld.so needs the "./" prefix or it always return false.
+# This is also how ldd calls it acc. to strace.
+if ld.so --verify ./gocryptfs ; then
 	echo "build-without-openssl.bash: error: compiled binary is not static"
 	exit 1
 fi
